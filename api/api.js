f2d7cdaecf1ebca1ecd7c7e0e68fb4f793da8769
Andrew Metell <andrew.metell@gmail.com>
2020-01-20 22:03:26 -0500

    Complete Expresso Assignment

--- api/api.js
+++ api/api.js
@@ -3,6 +3,11 @@ module.exports = apiRouter;
 
 apiRouter.get('/menus', (req, res, next) => {
     res.send('At API get route');
-    console.log('At API get route...');
 });
+
+const employeeRouter = require('./employee');
+apiRouter.use('/employee', employeeRouter);
+
+const menuRouter = require('./menu');
+apiRouter.use('/menu', menuRouter);
 
